6325820a7a509f82cba64a0a78cc0499b3d43cbf
TaobotX11 <eudor.olshop@gmail.com>
2026-05-26 08:43:50 +0700

    test: Add ASSERT_DEBUG_LOG to unit test framework #16540

--- src/noui.h
+++ src/noui.h
@@ -19,10 +19,10 @@ void noui_InitMessage(const std::string& message);
 /** Connect all nusacoind signal handlers */
 void noui_connect();
 
-/** Suppress all nusacoind signal handlers. Used to suppress output during test runs that produce expected errors */
-void noui_suppress();
+/** Redirect all nusacoind signal handlers to LogPrintf. Used to check or suppress output during test runs that produce expected errors */
+void noui_test_redirect();
 
-/** Reconnects the regular Non-GUI handlers after having used noui_suppress */
+/** Reconnects the regular Non-GUI handlers after having used noui_test_redirect */
 void noui_reconnect();
 
 #endif // BITCOIN_NOUI_H
